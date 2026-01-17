d044d30987c5ca00b338612c12d6e71b411defbb
joseleonec <joleonca@gmail.com>
2026-01-17 10:28:21 -0500

    Remove validation annotations and methods from MovimientoDTO for simplification

--- src/main/java/co/com/sofka/luchotest/dto/MovimientoDTO.java
+++ src/main/java/co/com/sofka/luchotest/dto/MovimientoDTO.java
@@ -3,28 +3,18 @@ package co.com.sofka.luchotest.dto;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record MovimientoDTO (
     
-    @NotNull
     LocalDateTime fecha,
 
-    @NotBlank
     String tipoMovimiento,
 
-    @NotNull
     BigDecimal valor,
 
-    @NotNull
+    BigDecimal saldo,
+
     Long cuentaId
 ) {
-
-    @AssertTrue(message = "El valor no puede ser cero")
-    public boolean isValorNotZero() {
-        return valor != null && valor.compareTo(BigDecimal.ZERO) != 0;
-    }
 
 }
