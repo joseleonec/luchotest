3c377c0ca51876a9627554fc0673ed8d1de432ea
joseleonec <joleonca@gmail.com>
2026-01-18 12:36:06 -0500

    refactor: Remove unused tables and data for persona and cliente from SQL scripts

--- microservice-cuentas/src/main/resources/db/data.sql
+++ microservice-cuentas/src/main/resources/db/data.sql
@@ -2,22 +2,6 @@
 SET statement_timeout = 0;
 SELECT pg_catalog.set_config('search_path', '', false);
 
--- Datos: persona
-INSERT INTO public.persona (edad, id, direccion, genero, identificacion, nombre, telefono) 
-VALUES (30, 1, 'Otavalo sn y principal', 'Masculino', '000000001', 'Jose Lema', '098254785') ON CONFLICT (id) DO NOTHING;
-INSERT INTO public.persona (edad, id, direccion, genero, identificacion, nombre, telefono) 
-VALUES (30, 2, 'Amazonas y NNUU', 'Femenino', '000000002', 'Marianela Montalvo', '097548965') ON CONFLICT (id) DO NOTHING;
-INSERT INTO public.persona (edad, id, direccion, genero, identificacion, nombre, telefono) 
-VALUES (30, 3, '13 junio y Equinoccial', 'Masculino', '000000003', 'Juan Osorio', '098874587') ON CONFLICT (id) DO NOTHING;
-
--- Datos: cliente
-INSERT INTO public.cliente (id, cliente_id, contrasena, estado) 
-VALUES (1, 'joselema', '1234', 'True') ON CONFLICT (id) DO NOTHING;
-INSERT INTO public.cliente (id, cliente_id, contrasena, estado) 
-VALUES (2, 'marianelamontalvo', '5678', 'True') ON CONFLICT (id) DO NOTHING;
-INSERT INTO public.cliente (id, cliente_id, contrasena, estado) 
-VALUES (3, 'juanosorio', '1245', 'True') ON CONFLICT (id) DO NOTHING;
-
 -- Datos: cuenta
 INSERT INTO public.cuenta (saldo_disponible, saldo_inicial, cliente_id, id, estado, numero_cuenta, tipo_cuenta) 
 VALUES (1000.00, 1000.00, 1, 5, 'True', '585545', 'Corriente') ON CONFLICT (id) DO NOTHING;
@@ -43,5 +27,3 @@ VALUES (0.00, 540.00, -540.00, 4, '2026-01-18 09:21:00.039209', 4, 'RETIRO') ON 
 -- Sincronización de Secuencias
 SELECT pg_catalog.setval('public.cuenta_id_seq', 5, true);
 SELECT pg_catalog.setval('public.movimiento_id_seq', 4, true);
-SELECT pg_catalog.setval('public.persona_seq', 51, true);
-
